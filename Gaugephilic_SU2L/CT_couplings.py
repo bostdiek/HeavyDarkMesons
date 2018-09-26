f05e947dfdc39bec385421b306c4b263a49c6f84
bostdiek <bostdiek@gmail.com>
2018-09-26 11:07:40 -0700

    Changed file names

---
+++ Gaugephilic_SU2L/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules 2.3.24
+# Mathematica version: 10.3.0 for Mac OS X x86 (64-bit) (October 9, 2015)
+# Date: Mon 24 Sep 2018 14:58:33
+
+
+from object_library import all_couplings, Coupling
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
